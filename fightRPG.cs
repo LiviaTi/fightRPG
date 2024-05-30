e97f324a03e5959aa452ddc8464ecaab0a6418e9
liviaTI <liviasantferreira@gmail.com>
2024-05-30 15:12:13 -0300

    Better code

--- fightRPG.cs
+++ fightRPG.cs
@@ -5,45 +5,26 @@ In this challenge, we will summarize the essentials of this interaction.
   If the monster has life (greater than zero), it will, in turn, attack the hero.
   As long as the hero and monster have health (greater than zero), the battle will continue. 
 */
+int hero = 10;
+int monster = 10;
 
-    int lifeHero = 100;
-    int lifeMonster = 100;
-    Random  attackHero = new Random();
-    int currentAttackHero = attackHero.Next(1, 51);
-    Random attackMonster = new Random();
-    int currentAttackMonster = attackMonster.Next(1, 51); 
-    bool currentHero = true;
-    bool currentMonster = false;
+Random dice = new Random();
 
-    while((lifeMonster >= 0) &&  (lifeHero >=0))
-    {
-        if(currentHero == true )
-        {
-            currentAttackHero = attackHero.Next(1, 51);
-            lifeMonster = lifeMonster - currentAttackHero;
-            currentHero = false;
-            currentMonster = true;
+do
+{
+    int roll = dice.Next(1, 11);
+    monster -= roll;
+    Console.WriteLine($"Monster was damaged and lost {roll} health and now has {monster} health.");
 
-            Console.WriteLine($" O Herói causou dano de {currentAttackHero} a vida do monstro é de:{lifeMonster}");
-            continue;
-        }
+    if (monster <= 0) continue;
 
-        if(currentMonster == true )
-        {
-            currentAttackMonster = attackMonster.Next(1, 51);
-            lifeHero = lifeMonster - currentAttackHero;
-            currentHero = true;
-            currentMonster = false;
+    roll = dice.Next(1, 11);
+    hero -= roll;
+    Console.WriteLine($"Hero was damaged and lost {roll} health and now has {hero} health.");
 
-            Console.WriteLine($" O Monstro causou dano de {currentAttackMonster} a vida do Herói é de:{lifeHero}");
-            continue;            
-        }
-      
-    }
+} while (hero > 0 && monster > 0);
 
-     if(lifeHero <= 0) Console.WriteLine("O herói morreu. Fim de jogo"); 
-    
-    if(lifeMonster <= 0) Console.WriteLine("O monstro morreu. Fim de jogo"); 
+Console.WriteLine(hero > monster ? "Hero wins!" : "Monster wins!");
    
     
     
